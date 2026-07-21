2c4f02254ae0007453d1f8fc1bf3ac6812043b8c
Runcheng Luo <119753362+runkko@users.noreply.github.com>
2026-07-21 12:15:02 +0800

    Deploy synthetic donut click handling

---
+++ _next/static/chunks/14adn2y_jr9y0.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,62961,t=>{t.v(e=>Promise.all(["static/chunks/0efh94x~s-b~x.js"].map(e=>t.l(e))).then(()=>e(44599)))}]);
